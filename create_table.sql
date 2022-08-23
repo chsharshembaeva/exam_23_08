9b875500557f3e20e9f82cd2de49aaa1735ec84b
chsharshembaeva <chopona09@gmail.com>
2022-08-23 21:33:34 +0600

    SecondCommit

--- create_table.sql
+++ create_table.sql
@@ -7,21 +7,3 @@ CREATE TABLE payments
 	Pay_date DATE
 	
 )
-
-(
-    Id SERIAL PRIMARY KEY,
-	FirstName CHARACTER VARYING(30) NOT NULL,
-    CustomerId INTEGER DEFAULT 1,
-    Quantity INTEGER,
-	Phone VARCHAR(30) UNIQUE NOT NULL,
-	TotalWeight NUMERIC(9,2),
-    FOREIGN KEY (CustomerId) REFERENCES Customers (Id) ON DELETE SET DEFAULT
---  FOREIGN KEY (CustomerId) REFERENCES Customers (Id) ON DELETE CASCADE
--- 	FOREIGN KEY (CustomerId) REFERENCES Customers (Id) ON DELETE SET NULL
-	PRIMARY KEY(OrderId, ProductId)
-	UNIQUE(Email)
-	UNIQUE(Phone) -- otdelno kajdiy
-	UNIQUE(Email, Phone)  -- v svyazke vmeste budut unique
-	CHECK((Age >0 AND Age<100) AND (Email !='') AND (Phone !=''))
-
-);ProductId INTEGER NOT NULL REFERENCES Products(Id) ON DELETE CASCADE,
